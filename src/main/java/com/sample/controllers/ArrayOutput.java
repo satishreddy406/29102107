2ea511cb5c3e920675b66d35903c5528130a9e87
satishreddy406 <satish06406@gmail.com>
2017-10-29 10:28:40 +0530

    Add files via upload

---
+++ src/main/java/com/sample/controllers/ArrayOutput.java
@@ -0,0 +1,14 @@
+package com.sample.controllers;
+
+public class ArrayOutput {
+
+	public static void main(String[] args) {
+		  int array_variable [] = new int[10];
+		    for (int i = 0; i < 10; ++i) {
+	                array_variable[i] = i;
+	                System.out.print(array_variable[i] + " ");
+	                i++;
+	            }
+	}
+
+}
